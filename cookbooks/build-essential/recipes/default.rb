ca9f543ba685b45a1ada03163413c0796e0211a1
Anbazhagan <anbublacky@gmail.com>
2015-08-03 20:26:01 +0530

    trying from first

--- cookbooks/build-essential/recipes/default.rb
+++ cookbooks/build-essential/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+include_recipe "apt"
 begin
   include_recipe "build-essential::_#{node['platform_family']}"
 rescue Chef::Exceptions::RecipeNotFound
